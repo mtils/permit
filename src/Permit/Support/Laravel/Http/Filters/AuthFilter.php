a4c6c2309e5322e9842eb0b97b0824ffdad449dd
Michael Tils <mtils@web-utils.de>
2014-11-16 22:03:40 +0100

    Added filter stub

---
+++ src/Permit/Support/Laravel/Http/Filters/AuthFilter.php
@@ -0,0 +1,16 @@
+<?php namespace Permit\Support\Laravel\Http\Filters;
+
+class AuthFilter{
+
+    public function __construct(){
+    }
+
+    public function checkPermission(){
+    
+    }
+
+    public function guest(){
+        echo "Asked";
+    }
+
+}
