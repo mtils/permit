5711b7f589462667f1e0cb03421c364002a19102
Michael Tils <mtils@web-utils.de>
2015-04-14 14:38:58 +0200

    Implemented a container for Laravel Guard
    Fixes #8

---
+++ src/Permit/Support/Laravel/CurrentUser/GuardContainer.php
@@ -0,0 +1,77 @@
+<?php namespace Permit\Support\Laravel\CurrentUser;
+
+
+use Illuminate\Auth\Guard;
+
+use Permit\CurrentUser\ContainerInterface;
+use Permit\CurrentUser\CanRememberUser;
+use Permit\User\UserInterface;
+
+class GuardContainer implements ContainerInterface, CanRememberUser
+{
+
+    /**
+     * @var \Illuminate\Auth\Guard
+     **/
+    protected $guard;
+
+    public function __construct(Guard $guard)
+    {
+        $this->guard = $guard;
+    }
+
+    /**
+     * @brief Retrieve the current user.
+     *
+     * @return \Permit\User\UserInterface
+     **/
+    public function user()
+    {
+        return $this->guard->user();
+    }
+
+    /**
+     * @brief Set the current user. If a user should be logged in as a
+     *        different user you shoul simply set a user a second time
+     *
+     * @param \Permit\User\UserInterface $user
+     * @param bool $persist Persists the user (in session)
+     * @return Permit\User\UserInterface
+     **/
+    public function setUser(UserInterface $user, $persist=true)
+    {
+        if(!$persist){
+            $this->guard->setUser($user);
+            return $user;
+        }
+
+        $this->guard->login($user, false);
+
+        return $user;
+
+    }
+
+    /**
+     * Set the user and produce a remember token
+     *
+     * @param \Permit\User\UserInterface
+     * @return \Permit\User\UserInterface
+     **/
+    public function setAndRemember(UserInterface $user)
+    {
+        $this->guard->login($user, true);
+        return $user;
+    }
+
+    /**
+     * @brief Sets the user to null
+     *
+     * @return bool
+     **/
+    public function clearUser()
+    {
+        $this->guard->logout();
+        return true;
+    }
+
+}
---
+++ tests/unit/Support/Laravel/CurrentUser/GuardContainerTest.php
@@ -0,0 +1,122 @@
+<?php 
+
+use Mockery as m;
+
+use Permit\Support\Laravel\CurrentUser\GuardContainer;
+use Permit\User\GenericUser;
+
+class GuardContainerTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testImplementsInterfaces()
+    {
+        $container = $this->newContainer();
+
+        $this->assertInstanceOf(
+            'Permit\CurrentUser\ContainerInterface',
+            $container
+        );
+
+        $this->assertInstanceOf(
+            'Permit\CurrentUser\CanRememberUser',
+            $container
+        );
+    }
+
+    public function testUserForwardsToGuard(){
+
+        $guard = $this->mockGuard();
+        $container = $this->newContainer($guard);
+        $user = $this->newUser();
+
+        $guard->shouldReceive('user')
+              ->once()
+              ->andReturn($user);
+
+        $this->assertSame($user, $container->user());
+    }
+
+    public function testSetUserForwardsToGuardIfNotPersisting()
+    {
+        $guard = $this->mockGuard();
+        $container = $this->newContainer($guard);
+        $user = $this->mockUser();
+
+        $guard->shouldReceive('setUser')
+              ->with($user)
+              ->once();
+
+        $this->assertSame($user, $container->setUser($user, false));
+    }
+
+    public function testSetUserForwardsToLoginIfPersisting()
+    {
+        $guard = $this->mockGuard();
+        $container = $this->newContainer($guard);
+        $user = $this->mockUser();
+
+        $guard->shouldReceive('login')
+              ->with($user, false)
+              ->once();
+
+        $this->assertSame($user, $container->setUser($user, true));
+    }
+
+    public function testSetAndRememberForwardsToLogin()
+    {
+        $guard = $this->mockGuard();
+        $container = $this->newContainer($guard);
+        $user = $this->mockUser();
+
+        $guard->shouldReceive('login')
+              ->with($user, true)
+              ->once();
+
+        $this->assertSame($user, $container->setAndRemember($user));
+    }
+
+    public function testClearUserForwardsToGuard()
+    {
+        $guard = $this->mockGuard();
+        $container = $this->newContainer($guard);
+        $user = $this->mockUser();
+
+        $guard->shouldReceive('logout')
+              ->once();
+
+        $this->assertTrue($container->clearUser($user));
+    }
+
+    public function newContainer($guard=null)
+    {
+        $guard = $guard ?: $this->mockGuard();
+        return new GuardContainer($guard);
+    }
+
+    public function mockGuard()
+    {
+        return m::mock('Illuminate\Auth\Guard');
+    }
+
+    public function newUser($id=1)
+    {
+        $user = new GenericUser;
+        $user->setAuthId($id);
+        return $user;
+    }
+
+    public function mockUser()
+    {
+        return m::mock(
+            'Permit\User\UserInterface',
+            'Illuminate\Auth\UserInterface'
+        );
+
+    }
+
+    public function tearDown()
+    {
+        m::close();
+    }
+
+}
