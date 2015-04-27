8801194541d78094510f635eec27cdca1e16ec66
Michael Tils <mtils@web-utils.de>
2015-04-27 07:50:55 +0200

    Added missing interfaces

--- src/Permit/Support/Laravel/User/EloquentUser.php
+++ src/Permit/Support/Laravel/User/EloquentUser.php
@@ -8,6 +8,8 @@ use Illuminate\Contracts\Auth\CanResetPassword as ResetPasswordInterface;
 use Illuminate\Auth\Passwords\CanResetPassword;
 
 use Permit\User\UserInterface as PermitUserInterface;
+use Permit\Permission\Holder\HolderInterface;
+use Permit\Permission\Holder\NestedHolderInterface;
 use Permit\Support\Laravel\Groups\EloquentHolderTrait;
 use Permit\Support\Laravel\Permission\Holder\EloquentJsonPermissionsTrait;
 use Permit\Support\Laravel\Registration\ActivatableByDate;
@@ -16,7 +18,9 @@ use Permit\Registration\ActivatableInterface;
 class EloquentUser extends Model implements Authenticatable,
                                             ResetPasswordInterface,
                                             PermitUserInterface,
-                                            ActivatableInterface
+                                            ActivatableInterface,
+                                            HolderInterface,
+                                            NestedHolderInterface
 {
 
     use UserTrait;
