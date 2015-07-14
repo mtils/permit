ad5c47e7be6f4a7f1a5468d74273e0310e9571a5
Michael Tils <mtils@web-utils.de>
2015-07-14 12:19:29 +0200

    Added OpenSSL Encrypter for Laravel

---
+++ src/Permit/Support/Laravel/Encryption/OpenSSLEncrypter.php
@@ -0,0 +1,183 @@
+<?php namespace Permit\Support\Laravel\Encryption;
+
+use Illuminate\Contracts\Encryption\Encrypter;
+
+class OpenSSLEncrypter implements Encrypter
+{
+
+    /**
+     * The second half of the method parameter of openssl_encrypt
+     * (e.g cbc for aes-128-cbc)
+     *
+     * @var string
+     **/
+    protected $mode = 'cbc';
+
+    /**
+     * The first half of the method parameter of openssl_encrypt
+     *
+     * @var string
+     **/
+    protected $cipher = 'aes-128';
+
+    /**
+     * The password parameter of openssl_encrypt
+     *
+     * @var string
+     **/
+    protected $key;
+
+    /**
+     * The iv, if not set it will be generated
+     *
+     * @var string
+     **/
+    protected $iv;
+
+    /**
+     * The key as a hex number
+     *
+     * @var string
+     **/
+    protected $encodedKey;
+
+
+    public function __construct($key)
+    {
+        $this->key = $key;
+    }
+
+    /**
+     * Encrypt the given value.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function encrypt($value)
+    {
+        $iv = $this->getIv();
+        $method = $this->method();
+        $password = $this->encodedKey();
+        $value = serialize($value);
+
+        $encrypted = openssl_encrypt($value, $method, $password, true, $iv);
+
+        return base64_encode($encrypted);
+
+    }
+
+    /**
+     * Decrypt the given value.
+     *
+     * @param  string  $payload
+     * @return string
+     */
+    public function decrypt($payload)
+    {
+        $iv = $this->getIv();
+        $method = $this->method();
+        $password = $this->encodedKey();
+        return unserialize(openssl_decrypt($payload, $method, $password, false, $iv));
+    }
+
+    /**
+     * Set the encryption mode.
+     *
+     * @param  string  $mode
+     * @return void
+     */
+    public function setMode($mode)
+    {
+        $this->mode = $mode;
+    }
+
+    /**
+     * Set the encryption cipher.
+     *
+     * @param  string  $cipher
+     * @return void
+     */
+    public function setCipher($cipher)
+    {
+        $this->cipher = $cipher;
+    }
+
+    public function method()
+    {
+        return implode('-', [$this->cipher, $this->mode]);
+    }
+
+    public function encodedKey()
+    {
+        if (!$this->encodedKey) {
+            $this->encodedKey = $this->toEncryptionKey($this->key, $this->keyLength());
+        }
+        return $this->encodedKey;
+    }
+
+    public function toEncryptionKey($string, $length, $encoding=null)
+    {
+        $encoding = $encoding ?: mb_internal_encoding();
+        $hexString = $this->stringToHex($string, $encoding);
+        $hexStringLength = strlen($hexString);
+
+        if ($hexStringLength == $length) {
+            return $hexString;
+        }
+
+        if ($hexStringLength > $length) {
+            return substr($hexString, 0, $length);
+        }
+
+        $factor = (int)ceil(min($length/$hexStringLength, $length));
+
+        return substr(str_repeat($hexString, $factor), 0, $length);
+
+    }
+
+    public function stringToHex($string)
+    {
+        $hex='';
+        foreach ($this->splitIntoChars($string) as $char) {
+            $hex .= strtoupper(dechex(ord($char)));
+        }
+        return $hex;
+    }
+
+    public function splitIntoChars($string, $encoding=null)
+    {
+
+        $encoding = $encoding ?: mb_internal_encoding();
+        $strlen = mb_strlen($string, $encoding);
+        $splittedString = [];
+
+        while ($strlen) {
+            $splittedString[] = mb_substr($string, 0, 1, $encoding);
+            $string = mb_substr($string, 1, $strlen, $encoding);
+            $strlen = mb_strlen($string, $encoding);
+        }
+
+        return $splittedString;
+
+    }
+
+    public function keyLength()
+    {
+        return openssl_cipher_iv_length($this->method());
+    }
+
+    public function getIv()
+    {
+        if ($this->iv === null) {
+            $this->iv = strrev($this->encodedKey());
+        }
+        return $this->iv;
+
+    }
+
+    public function setIv($iv)
+    {
+        $this->iv = $iv;
+    }
+
+}
---
+++ tests/unit/Support/Laravel/Encryption/OpenSSLEncrypterTest.php
@@ -0,0 +1,120 @@
+<?php 
+
+use Mockery as m;
+use Permit\Support\Laravel\Encryption\OpenSSLEncrypter;
+
+class OpenSSLEncrypterTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testImplementsInterface()
+    {
+        $this->assertInstanceOf(
+            'Illuminate\Contracts\Encryption\Encrypter',
+            $this->newEncrypter()
+        );
+    }
+
+    public function testConvertToHexConvertsCorrectly()
+    {
+        $encrypter = $this->newEncrypter();
+
+        $this->assertEquals('3132333435363738', $encrypter->stringToHex('12345678'));
+        $this->assertEquals('E2', $encrypter->stringToHex('€'));
+
+    }
+
+    public function testToEncryptionKeyReturnsUnPaddedStringIfLengthMatches()
+    {
+        $encrypter = $this->newEncrypter();
+        $this->assertEquals('3132333435363738', $encrypter->toEncryptionKey('12345678', 16));
+        $this->assertEquals('E2', $encrypter->toEncryptionKey('€', 2));
+    }
+
+    public function testToEncryptionKeyReturnsPaddedStringIfTooLong()
+    {
+        $encrypter = $this->newEncrypter();
+        $this->assertEquals('31323334', $encrypter->toEncryptionKey('12345678', 8));
+        $this->assertEquals('E', $encrypter->toEncryptionKey('€', 1));
+    }
+
+    public function testToEncryptionKeyReturnsRepeatedStringIfTooShort()
+    {
+        $encrypter = $this->newEncrypter();
+        $this->assertEquals('313233343536373831323334', $encrypter->toEncryptionKey('12345678', 24));
+        $this->assertEquals('E2E2E2E', $encrypter->toEncryptionKey('€', 7));
+    }
+
+    public function testStringEncryption()
+    {
+
+        $encrypter = $this->newEncrypter();
+
+        $tests = ['Foo-Bar','#**#+??00=)7899', 'öÄßÄli€'];
+
+        foreach ($tests as $test) {
+            $encrypted = $encrypter->encrypt($test);
+            $this->assertNotEquals($encrypted, $test);
+            $this->assertEquals($test, $encrypter->decrypt($encrypted));
+        }
+
+    }
+
+    public function testBoolEncryption()
+    {
+
+        $encrypter = $this->newEncrypter();
+
+        $tests = [true, false];
+
+        foreach ($tests as $test) {
+            $encrypted = $encrypter->encrypt($test);
+            $this->assertNotEquals($encrypted, $test);
+            $this->assertTrue($test === $encrypter->decrypt($encrypted));
+        }
+
+    }
+
+    public function testFloatEncryption()
+    {
+
+        $encrypter = $this->newEncrypter();
+
+        $tests = [26.4, 28.7];
+
+        foreach ($tests as $test) {
+            $encrypted = $encrypter->encrypt($test);
+            $this->assertNotEquals($encrypted, $test);
+            $this->assertTrue($test === $encrypter->decrypt($encrypted));
+        }
+
+    }
+
+    public function testArrayEncryption()
+    {
+
+        $encrypter = $this->newEncrypter();
+
+        $test = ['this'=>'is', 'a'=>'test'];
+
+        $encrypted = $encrypter->encrypt($test);
+        $this->assertNotEquals($encrypted, $test);
+        $this->assertEquals($test, $encrypter->decrypt($encrypted));
+
+    }
+
+    protected function newEncrypter($key='9nwincrye3fidbAh')
+    {
+        return new OpenSSLEncrypter($key);
+    }
+
+    public function setUp()
+    {
+        mb_internal_encoding('UTF-8');
+    }
+
+    public function tearDown()
+    {
+        m::close();
+    }
+
+}
