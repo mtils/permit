e988f6083ed1c990821bd19f7ab237efad558821
Michael Tils <mtils@web-utils.de>
2016-06-02 10:29:17 +0200

    Added missing interface

--- src/Permit/Support/Laravel/Groups/EloquentGroup.php
+++ src/Permit/Support/Laravel/Groups/EloquentGroup.php
@@ -5,11 +5,14 @@ use Illuminate\Database\Eloquent\Model;
 use Permit\Support\Laravel\Permission\Holder\EloquentJsonPermissionsTrait;
 use Permit\Permission\Holder\HolderInterface;
 use Permit\Groups\GroupInterface;
+use Permit\Permission\PermissionableInterface;
+use Permit\Permission\Holder\PermissionableHolderTrait;
 
-class EloquentGroup extends Model implements HolderInterface, GroupInterface
+class EloquentGroup extends Model implements HolderInterface, GroupInterface, PermissionableInterface
 {
 
     use EloquentJsonPermissionsTrait;
+    use PermissionableHolderTrait;
 
     /**
      * {@inheritdoc}
